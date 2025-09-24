403150f1bfb9ae8b51c1c7d220ea799c06f504de
dianahnatiuk <hnatiukdi@gmail.com>
2025-09-24 21:13:36 +0300

    add b

--- main.cpp
+++ main.cpp
@@ -1,10 +1,32 @@
+#include <iostream>
+
 int main (){
 int a = 0;
 int b = 0;
+int PI;
+    int a1;
+    int first_name;
+    int lastName;
+    first_name = 1;
+    std::cout << first_name << "\n";
+    first_name = 2.5;
+    std::cout << first_name << "\n";
+    first_name = true;
+    std::cout << first_name << "\n";
+    first_name = false;
+    std::cout << first_name << "\n";
+    first_name = 'a';
+    std::cout << first_name << "\n";
+    first_name = 1 + 2.5;
+    std::cout << first_name << "\n";
+
+   std::string name = "Diana";
+    std::cout << name << "\n";
+ name = name + "1";
+    std::cout << name << "\n";
+    double price = 0
 
 
 
-
-return 0;
 }
 
