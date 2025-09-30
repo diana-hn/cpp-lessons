112d6a1b5bc83d15eced6d1ed6f3dcc502282844
dianahnatiuk <hnatiukdi@gmail.com>
2025-09-30 21:58:59 +0300

    mult7

--- main.cpp
+++ main.cpp
@@ -1,36 +1,12 @@
 #include <iostream>
 
-int one ();
-
-int two(int x = 2) {
-    return x;
-}
-
-namespace functions {
-    namespace a {
-        int two(int x = 2) {
-            return x;
-        }
+int main(int x) {
+    int i = 1;
+    while (i < 11) {
+        x = 7;
+        std::cout << i * x << std::endl;
+        i++;
     }
 
-    int two1 (int x = 3) {
-        return x;
-    }
 
-}
-
-using namespace functions;
-
-int main() {
-    one();
-    two(5);
-    two();
-    int x;
-    std::cout << "Hello World!" << std::endl;
-    two1();
-
-    return 0;
-}
-int one() {
-    return 1;
 }
