9be24d08ec5e05ad2652d7226efc68f66d7a59f6
dianahnatiuk <hnatiukdi@gmail.com>
2025-10-16 22:22:56 +0300

    Implement minMax fn.

--- main.cpp
+++ main.cpp
@@ -10,10 +10,27 @@ void increment(int &a) {
     a++;
 }
 
+void sum_arr(int a[5], int &sum) {
+    for(int i = 0; i < 5; i++ ) {
+        sum += a[i];
+    }
+}
+
+void find_min_max(const int arr[5], int &min, int &max) {
+    for (int i = 0; i < 5; i++) {
+        int current = arr[i];
+        min = min > current ? current: min;
+        max = max < current ? current: max;
+    }
+
+}
+
 int main() {
-   int a = 5;
-    std::cout << "before increment " << a << std::endl;
-    increment(a);
-    std::cout << "after increment " << a << std::endl;
+    int arr[5] = {10, 2, 3, 41, 5};
+    int max = arr[0];
+    int min = arr[0];
+    std::cout << "before sum_arr " << min << ", " << max << std::endl;
+    find_min_max(arr, min, max);
+    std::cout << "after sum_arr " << min << ", " << max << std::endl;
     return 0;
 }
